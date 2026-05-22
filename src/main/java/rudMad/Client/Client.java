77d095b8d72ebab1d089f36dd1313de9007f7356
rudmad13 <madrigalrudy34@gmail.com>
2026-05-22 01:09:27 -0700

    Added Functionality to Client class. Client is now able to send messages to the server

--- src/main/java/rudMad/Client/Client.java
+++ src/main/java/rudMad/Client/Client.java
@@ -1,20 +1,44 @@
 package rudMad.Client;
 
+
+import java.util.Scanner;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+
 import java.net.Socket;
 
 public class Client {
 
     private Socket client;
+    Scanner scanner;
 
 
     public void start(int port){
 
-
+        scanner = new Scanner(System.in);
         try{
+
         client = new Socket("LocalHost", port);
 
         System.out.println("connection is successful");
+
+        BufferedWriter out = new BufferedWriter( new OutputStreamWriter(client.getOutputStream()));
+        
+        String message = "";
+        while(!(message.equals("quit()"))){
+
+            System.out.println(">");
+            message = scanner.nextLine();
+            out.write(message);
+            out.newLine();
+            out.flush();
+
+
+
+        }
+
+        client.close();
 
         } catch (IOException e){
             e.printStackTrace();
--- src/main/java/rudMad/Server/Server.java
+++ src/main/java/rudMad/Server/Server.java
@@ -1,5 +1,9 @@
 package rudMad.Server;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+
 import java.net.ServerSocket;
 import java.net.Socket;
 public class Server {
@@ -10,13 +14,26 @@ public class Server {
         try{
 
         ServerSocket server = new ServerSocket(port);
-        System.out.println("Server is listening on"+ server.getLocalPort());
+        System.out.println("Server is listening on "+ server.getLocalPort());
 
         Socket client = server.accept();
-        System.out.println("Connection made" + client.getInetAddress());
+        System.out.println("Connection made " + client.getInetAddress());
 
+        //Take in the Input and Output buffer
+        BufferedReader in =  new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+        String message;
+        //Keep reading while the connection is open.
+        while((message = in.readLine()) != null){
+
+            System.out.println(message);
+
+        }
 
         server.close();
+
+        System.out.println("Connection is closed");
+
         } catch(IOException e){
             e.printStackTrace();
         }
